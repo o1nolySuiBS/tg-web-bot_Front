5685f6d06598743a58194023f090a58992101656
ihor1 <ihor1006@gmail.com>
2024-07-18 13:34:48 +0200

    ver +form fixed

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <div className="App">
-
+            <Header/>
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path='/form' element={<Form />}/>
