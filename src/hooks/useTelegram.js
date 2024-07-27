7b86e69f6057d1b71c898a6d75801255de4e9ec7
ihor1 <ihor1006@gmail.com>
2024-07-28 00:55:41 +0200

    ver ver 2.2  front done fixed

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -2,9 +2,11 @@ const tg = window.Telegram.WebApp;
 
 export function useTelegram(){
 
+
     const onClose = () =>{
         tg.close()
     }
+
 
     const onToggleButton = () => {
         if (tg.MainButton.isVisible){
